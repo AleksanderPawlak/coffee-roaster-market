5e6518c5e9bc512828160cb4471b480a9cdeb06f
AleksanderPawlak <aleksander.pawlak1996@gmail.com>
2021-03-11 23:07:19 +0100

    black step + draft of tests step

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,19 @@
-from nox_poetry import session
+import nox_poetry
 
 
-@session
+@nox_poetry.session
 def lint(session):
     session.install("flake8")
     session.run("flake8", "coffee_roaster_market", "manage.py")
+
+
+@nox_poetry.session
+def black(session):
+    session.install("black")
+    session.run("black", "--check", "coffee_roaster_market")
+
+
+@nox_poetry.session
+def tests(session):
+    session.run("poetry", "install", external=True)
+    # session.run('pytest')  # TODO: run pytest
