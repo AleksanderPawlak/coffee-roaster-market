ddb25bad1ed19b14921f35b2cb39d7e59a15f53f
AleksanderPawlak <aleksander.pawlak1996@gmail.com>
2021-04-10 21:26:45 +0200

    django environ with Dockerfile and docker-compose

--- coffee_roaster_market/settings.py
+++ coffee_roaster_market/settings.py
@@ -10,20 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR / ".env"
 
+env = environ.Env(DEBUG=(bool, False))
+env.read_env(env.str("ENV_PATH", str(ENV_FILE)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "39o8mr8a(ccn+jele=v90bl_d)(buk3q0y0ke&&7f-pol7tw*="
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
