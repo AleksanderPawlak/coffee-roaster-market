9ee522af4fb159b6827eeca6924f993223b90edd
kuchichan <pawel.kucharski@hotmail.com>
2021-06-07 21:49:22 +0200

    Alignment for changes

--- coffee_roaster_market/urls.py
+++ coffee_roaster_market/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
-    path("", include("coffee_roaster_market.apps.coffee.urls", namespace="coffee")),
-    path("item", include("coffee_roaster_market.apps.item.urls", namespace="item")),
+    path("api/", include("coffee_roaster_market.apps.coffee.urls")),
+    path("api/", include("coffee_roaster_market.apps.item.urls")),
     path("admin/", admin.site.urls),
     path(
         "api/token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"
@@ -14,5 +14,5 @@ urlpatterns = [
     path(
         "api/token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"
     ),
-    path("api/", include("coffee_roaster_market.apps.account.urls")),
+    path("api/account/", include("coffee_roaster_market.apps.account.urls")),
 ]
